52be89392b5bc04f4d2a63e33c4c878e865c1a69
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-04-25 16:40:08 +0200

    auth

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Auth::routes();
+Route::group(['prefix' => 'admin','as' => 'admin.'], fn () => Auth::routes());
+
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
