3037089b46359efebcbdbd6602eb244a8544fd16
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2022-04-25 15:10:21 +0200

    created new table 4 2

--- database/migrations/2022_04_25_124344_create_voluntary_work_table.php
+++ database/migrations/2022_04_25_124344_create_voluntary_work_table.php
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('voluntary_works);
+        Schema::dropIfExists('voluntary_works');
     }
 };
