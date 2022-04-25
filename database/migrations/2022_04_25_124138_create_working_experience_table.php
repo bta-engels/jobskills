6a57bc974a5c2a087f312d69825255e6adc535ab
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2022-04-25 15:09:22 +0200

    created new table 4

---
+++ database/migrations/2022_04_25_124138_create_working_experience_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('working_experiences', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('customer_id');
+            $table->string('name',50);
+            $table->text('description')->nullable();
+            $table->date('from');
+            $table->date('until');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('working_experiences');
+    }
+};
