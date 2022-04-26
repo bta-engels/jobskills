7516e23a737188c34fb60b69d74aa54d5d82ddb5
digicommons <43313518+digicommons@users.noreply.github.com>
2022-04-26 13:04:50 +0200

    Add Czech localization

---
+++ lang/cs/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Přihlašovací údaje neodpovídají záznamům v databázi.',
+    'password' => 'Vámi zadané heslo je není správné.',
+    'throttle' => 'Příliš mnoho pokusů o příhlášení. Zkuste to znovu za :seconds vteřin.',
+
+];
