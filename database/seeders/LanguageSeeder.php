c3744fd5d404882c733294e6b1ccb8690fddbb05
digicommons <43313518+digicommons@users.noreply.github.com>
2022-04-26 11:23:45 +0200

    Add Language Seeder

---
+++ database/seeders/LanguageSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Language;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class LanguageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('languages')->truncate();
+        foreach (config('data.languages') as $name) {
+            Language::create(['name' => $name]);
+        }
+    }
+}
