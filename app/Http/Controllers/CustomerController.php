108474c3f34cc6665f885f7032c322d66875f2d3
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-04-27 10:27:52 +0200

    add Customer auth views, model, controller, request

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\UpdateCustomerRequest;
+use App\Models\Customer;
+use Illuminate\Http\Response;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StoreCustomerRequest $request
+     * @return Response
+     */
+    public function store(StoreCustomerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Customer $customer
+     * @return Response
+     */
+    public function show(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Customer $customer
+     * @return Response
+     */
+    public function edit(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param UpdateCustomerRequest $request
+     * @param Customer $customer
+     * @return Response
+     */
+    public function update(UpdateCustomerRequest $request, Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Customer $customer
+     * @return Response
+     */
+    public function destroy(Customer $customer)
+    {
+        //
+    }
+}
---
+++ resources/views/customers/auth/login.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-sm-12 col-md-8 col-lg-4">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                    <x-form method="POST" action="{{ route('login') }}">
+                        <x-form-input type="email" class="mb-3" name="email" label="{{ __('Email Address') }}" required autocomplete="email" autofocus />
+                        <x-form-input type="password" class="mb-3" name="password" label="{{ __('Password') }}" required autocomplete="current-password" />
+                        <x-form-checkbox name="remember" label="{{ __('Remember me') }}" />
+                        <x-form-submit class="mt-3">{{ __('Login') }}</x-form-submit>
+
+                        @if(Route::has('password.request'))
+                            <a class="btn btn-link mt-3" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                        @endif
+                    </x-form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
