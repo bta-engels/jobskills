f37d48048ba773d991c96d8f56a57867119274b8
memoray <102960499+memoray@users.noreply.github.com>
2022-04-28 08:56:46 +0200

    fixed routes to reset password

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use App\Http\Controllers\AuthCustomer\CustomerRegisterController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LocaleController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthCustomer;
+use App\Http\Controllers\AuthCustomer\CustomerConfirmPasswordController;
+use App\Http\Controllers\AuthCustomer\CustomerResetPasswordController;
+use App\Http\Controllers\AuthCustomer\CustomerForgotPasswordController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,6 +32,9 @@ Route::post('register', [CustomerRegisterController::class, 'register'])->name('
 Route::post('logout', [CustomerLoginController::class, 'logout'])->name('logout');
 Route::get('login', [CustomerLoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [CustomerLoginController::class, 'login'])->name('login');
+
+Route::get('password/reset', [CustomerForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('password/reset', [CustomerResetPasswordController::class, 'reset'])->name('password.update');
 
 Route::get('/lang/{locale}',[LocaleController::class,'set'])->name('locale');
 Route::get('/', [HomeController::class, 'index'])->name('home');
