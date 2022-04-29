7001739e2729f77645773f7d217f965f2f48c6ab
digicommons <43313518+digicommons@users.noreply.github.com>
2022-04-29 21:37:19 +0200

    Create profile info table in show.blade.php

--- resources/views/customers/profile/show.blade.php
+++ resources/views/customers/profile/show.blade.php
@@ -2,6 +2,36 @@
 
 @section('content')
     <h3>{{ $customer->name }}</h3>
+    <table class="table table-striped">
+        <tr>
+            <td>{{ __('First- and Lastname') }}:</td>
+            <td>{{ $customer->name }}</td>
+        </tr>
+        <tr>
+            <td>{{ __('Email Address') }}</td>
+            <td>{{ $customer->email }}</td>
+        </tr>
+        <tr>
+            <td>{{ __('Phone number') }}</td>
+            <td>{{ $customer->phone }}</td>
+        </tr>
+        <tr>
+            <td>{{ __('Street') }}</td>
+            <td>{{ $customer->street }}</td>
+        </tr>
+        <tr>
+            <td>{{ __('City') }}</td>
+            <td>{{ $customer->city }}</td>
+        </tr>
+        <tr>
+            <td>{{ __('Postal code') }}</td>
+            <td>{{ $customer->postcode }}</td>
+        </tr>
+        <tr>
+            <td>{{ __('Date of Birth') }}</td>
+            <td>{{ \Carbon\Carbon::parse($customer->birthdate)->format('d.m.Y') }}</td>
+        </tr>
+    </table>
     <div>
         <a class="btn btn-primary" href="{{ route('customers.edit', $customer) }}">
             {{ __('Change profile data') }}</a>
