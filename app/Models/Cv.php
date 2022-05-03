5b35e4dfe8c53bbea9db8f8a47d6d03f930dec75
memoray <102960499+memoray@users.noreply.github.com>
2022-05-03 14:17:35 +0200

    cvs crud

---
+++ app/Models/Cv.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cv extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $guarded = ['id'];
+
+}
