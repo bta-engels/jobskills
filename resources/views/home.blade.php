3d8c58bb9b1cee4b76f751428a2e89d9d583f2d9
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-05-03 11:25:47 +0200

    auth check

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    {{ __('This is my home page') }}
+    @auth('admin')
+        <h3>Admin</h3>
+    @elseauth('customer')
+        <h3>Kunde</h3>
+    @else
+        <h3>Gast</h3>
+    @endauth
+
+    <p>{{ __('This is my home page') }}</p>
 @endsection
