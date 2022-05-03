991939f4b4209436f3803c7810ce5f458a721567
memoray <102960499+memoray@users.noreply.github.com>
2022-05-03 11:50:21 +0200

    auth guard inserted in controller

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -35,6 +35,7 @@ class ConfirmPasswordController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth:admin');
     }
+
 }
