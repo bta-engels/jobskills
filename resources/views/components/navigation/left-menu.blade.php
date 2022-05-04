f8c66340bc06e0d99144cd3c9adc775bd00ff6b0
memoray <102960499+memoray@users.noreply.github.com>
2022-05-04 09:36:29 +0200

    deleted left menu file

--- resources/views/components/navigation/left-menu.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="navbar-light">
-    <ul class="navbar-nav">
-        @foreach(config('nav.cv') as $item)
-            <li class="navbar-item">
-                <a class="nav-link" href="">{{ __($item) }}</a>
-            </li>
-        @endforeach
-    </ul>
-</div>
