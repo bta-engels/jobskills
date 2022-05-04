331e6354a7d9dcbed9808f7d0a4addb9562ead23
memoray <102960499+memoray@users.noreply.github.com>
2022-05-04 12:43:25 +0200

    My CV button

--- resources/views/components/navigation/left.blade.php
+++ resources/views/components/navigation/left.blade.php
@@ -2,6 +2,13 @@
     <li class="nav-item">
         <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
     </li>
+    @auth('admin')
+
+    @elseauth('customer')
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('cv.personalDataEdit', auth('customer')->user()) }}">{{ __('My CV') }}</a>
+        </li>
+    @endauth
 
     <!-- auth checks -->
 </ul>
