fee5a60a6658cd7d41b4ed161cf579d030675381
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-05-12 12:26:56 +0200

    api login

---
+++ app/Http/Controllers/Api/ApiLoginController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class ApiLoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::whereEmail($request->email)->first();
+
+        if ( ! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['Login Daten sind nicht korrekt.'],
+            ]);
+        }
+
+        $token = $user->createToken($user->email . '-' .uniqid(), ['write'])->plainTextToken;
+        // json rückgabe: name, email, token
+        $response = [
+            'name'  => $user->name,
+            'email'  => $user->email,
+            'token'  => $token,
+        ];
+
+        return response()->json($response);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+use App\Http\Controllers\Api\ApiLoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', [ApiLoginController::class, 'login']);
+
+Route::resource('todos', TodoController::class);
