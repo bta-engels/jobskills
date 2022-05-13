88d837bc2fe302852c56069c7b9a234be5bdb185
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-05-13 16:31:12 +0200

    tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,31 @@
 
 namespace Tests;
 
+use App\Models\Customer;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Route;
 
 abstract class TestCase extends BaseTestCase
 {
+    /**
+     * @var Collection
+     */
+    protected $routes;
+
+    protected $customer = [
+        'name'      => 'Paul Panther',
+        'email'     => 'engels@f50.de',
+        'password'  => 'password',
+        'password_confirmation' => 'password',
+    ];
+
+    protected function setUp():void
+    {
+        parent::setUp();
+        $this->routes = collect(Route::getRoutes()->getRoutesByName());
+    }
+
     use CreatesApplication;
+
 }
