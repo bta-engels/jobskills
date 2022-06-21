a366b8208f31f9a218fc12bcdf0fe468703e6ec9
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-06-21 12:01:01 +0200

    add getID3 support for media analysing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
     {
         $getID3 = new getID3();
         $fileInfo = $getID3->analyze(base_path('docs/hey_bulldog.avi'));
-//        dd($fileInfo);
+        dd($fileInfo);
 //        dd($fileInfo['playtime_string']);
         return view('home');
     }
